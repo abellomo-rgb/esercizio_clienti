90cdd6140b85be1cff4eba08642618bbbde61495
abellomo-rgb <abellomo@studenti.laziodigital.it>
2026-03-19 12:28:34 +0100

    nuova funzione

---
+++ aggiunta_cliente_priorita.py
@@ -0,0 +1,31 @@
+import json
+
+with open ("clienti.json", "r", encoding="utf-8") as f:
+    dati = json.load(f)
+
+clienti = dati['dati']
+
+# Aggiungere il nuovo cliente PRIMA di ordinare
+nuovo_cliente = {
+    "nome": "Nuovo",
+    "cognome": "Cliente",
+    "eta": 19,
+    "patrimonio": 100,
+    "priorita": 6
+}
+
+clienti.append(nuovo_cliente)
+
+def sort_casereccio(items, key="priorita"):
+    for i in range(len(items)):
+        for j in range(len(items)):
+            if int(items[j][key]) < int(items[i][key]):
+                temp = items[i]
+                items[i] = items[j]
+                items[j] = temp
+    return items
+
+clienti_sorted = sort_casereccio(clienti, key="priorita")
+
+for persona in clienti_sorted:
+    print(f"{persona['nome']} {persona['cognome']} - Priorita: {persona['priorita']}")
